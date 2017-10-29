ea448329df1107198a97b9cc60d71888b3d7d4f4
malek sumera <malekssa22@gmail.com>
2017-10-29 18:56:37 +0200

    my first commit

---
+++ lab1.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.speed(10)
+turtle.pensize(999)
+for i in range(3):
+	turtle.left(20)
+	turtle.forward(120)
+	turtle.left(90)
+	turtle.forward(120)
+	turtle.left(90)
+	turtle.forward(120)
+	turtle.left(90)
+	turtle.forward(120)
+turtle.mainloop()   
